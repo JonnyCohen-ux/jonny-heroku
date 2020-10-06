ff577ffc5a00167bc6792974926f68f2d03fd818
JonnyCohen-ux <you@example.com>
2020-10-06 20:56:47 +0300

    a

--- app.js
+++ app.js
@@ -13,5 +13,4 @@ app.use(express.static(path.join(__dirname, "jonny-heroku/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve("jonny-heroku/dist/index.html"));
 });
-
 app.listen(PORT, () => console.log(`Server run on por ${PORT}`));
