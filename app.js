0fff546898fdfcde7234e0be67a5a09e7e90f99c
JonnyCohen-ux <you@example.com>
2020-10-06 20:50:00 +0300

    i

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname, "jonny-heroku/dist")));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "jonny-heroku/dist")));
 
-app.use(cors());
 app.get("*", (req, res) => {
   res.sendFile(path.resolve("jonny-heroku/dist/index.html"));
 });
