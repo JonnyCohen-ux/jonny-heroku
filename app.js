23aff948e6714bf4ea99f32686ef231e0dd66cb9
JonnyCohen-ux <you@example.com>
2020-10-06 20:27:52 +0300

    make it better

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
+// app.use(express.static(path.join(__dirname, "jonny-heroku/dist")));
 app.use(express.static(path.join(__dirname, "jonny-heroku/dist")));
 
 app.use(cors());
